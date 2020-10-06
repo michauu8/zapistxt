497e0390a295a4d3b29aaff47d99d7a1f8009e84
Michał Polewczyk <michalpolewczyk8@gmail.com>
2020-10-06 17:18:38 +0200

    zapisz se

---
+++ zapistxt/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace zapistxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextWriter txt = new StreamWriter("C:\\users\\Michał\\Projekty\\zapis.txt");
+            txt.Write(richTextBox1.Text);
+            txt.Close();
+        }
+    }
+}
